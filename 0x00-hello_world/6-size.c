2cd9ab381208402e6095965d342714d38a79d305
Johnson Johnson Enyenihi <johnenyenihi2017@gmail.com>
2022-09-12 02:24:43 -0700

    commit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,10 +8,15 @@
  */
 int main(void)
 {
-printf("size of a char: %ld byte(s)\n", sizeof(char));
-printf("size of an int: %ld byte(s)\n", sizeof(int));
-printf("size of a long int: %ld byte(s)\n", sizeof(long int));
-printf("size of a long long int: %ld byte(s)\n", sizeof(long long int));
-printf("size of a float: %ld byte(s)\n", sizeof(float));
+	char c;
+	int i;
+	long li;
+	long long lli;
+	float f;
+printf("size of a char: %ld byte(s)\n", sizeof(c));
+printf("size of an int: %ld byte(s)\n", sizeof(i));
+printf("size of a long int: %ld byte(s)\n", sizeof(li));
+printf("size of a long long int: %ld byte(s)\n", sizeof(lli));
+printf("size of a float: %ld byte(s)\n", sizeof(f));
 return (0);
 }
